ad384d53dc4cc380de0e7c07e7b849c1cb8c9133
Nicolas <nicolas.rolland@gmail.com>
2012-08-20 22:25:00 +0200

    adde weighted quick find

--- Algs4/Algs4/UnionStructure.fs
+++ Algs4/Algs4/UnionStructure.fs
@@ -38,3 +38,28 @@ type QuickFind (n:int) =
          entries.[root p] <- root q
    override x.ToString() = 
       entries.ToString()
+
+
+type WeightedQuickFind (n:int) = 
+   let entries = Array.init n id
+   let sizes  = Array.create n 0
+   
+   let rec root p = 
+         if entries.[p] = p then p
+         else
+            root entries.[p]
+
+   member x.IsConnected p q = root p = root q
+
+   member x.Union (p:int) (q:int) = 
+      if max p q > n - 1 then
+         failwithf "max capacity reached %A %A" (max p q) n
+      else
+         let proot, qroot = root p, root q 
+         let rootsmallest, rootbiggest = if sizes.[proot] < sizes.[qroot] then proot, qroot else qroot, proot
+         entries.[rootsmallest] <- rootbiggest
+         sizes.[rootbiggest]    <- sizes.[rootbiggest] + sizes.[rootsmallest]
+
+
+   override x.ToString() = 
+      entries.ToString()
--- Algs4/Algs4/tests.fs
+++ Algs4/Algs4/tests.fs
@@ -50,3 +50,26 @@ let ``QuickFind `` () =
 
             c2.IsConnected 5 4  = false@>
 
+
+[<Fact>]
+let ``WeightedQuickFind `` () =
+   let c2 = WeightedQuickFind(10)
+   c2.Union 4 3
+   c2.Union 3 8 // 0 1 2 8 35
+   c2.Union 6 5 
+   c2.Union 9 4
+   c2.Union 2 1
+   c2.Union 5 0
+   c2.Union 7 2
+   c2.Union 6 1
+
+   test <@  c2.IsConnected 4 3 &&
+            c2.IsConnected 3 9 &&
+            c2.IsConnected 4 4 &&
+
+            c2.IsConnected 6 7 &&
+            c2.IsConnected 5 7 &&
+            c2.IsConnected 0 2 &&
+
+            c2.IsConnected 5 4  = false@>
+
